fb03f1a1581849d288890d3d3e22209411ea69b2
christinalle <42782619+christinalle@users.noreply.github.com>
2019-11-15 14:59:51 -0500

    updated working API

--- api.py
+++ api.py
@@ -5,14 +5,36 @@ import sys
 
 app = Flask(__name__)
 
+# loading model needs to be its own method?
 sys.setrecursionlimit(3000)
 print("Loading NLP Model...")
-
+ 
 nlp_model = gensim.models.KeyedVectors.load_word2vec_format('/Users/christinalle/Desktop/GoogleNews-vectors-negative300.bin.gz', binary = True)
 
 print("NLP Model Loaded")
 
-@app.route('/get/<string>')
+# methods for getting string responses for debugging
+@app.route('/get/testword2vec/<string>')
+def word2vec_func_s(string):
+    word_array = string.split()
+    single_vector = np.zeros(300)
+    for word in word_array:
+        try:
+            single_vector += nlp_model.get_vector(word)
+        except:
+            continue
+    return str(single_vector)
+        # ''.join(single_vector)
+
+@app.route('/get/testfindfoodsim/<word>')
+def find_food_sim_s(word):
+    if nlp_model.similarity('edible', word) > .1 and nlp_model.similarity('food', word) > .15:
+        return 'true'
+    else:
+        return 'false'
+
+# methods for getting expected type responses for debugging
+@app.route('/get/word2vec/<string>')
 def word2vec_func(string):
     word_array = string.split()
     single_vector = np.zeros(300)
@@ -23,16 +45,17 @@ def word2vec_func(string):
             continue
     return single_vector
 
-@app.route('/get/<word>')
+@app.route('/get/findfoodsim/<word>')
 def find_food_sim(word):
     if nlp_model.similarity('edible', word) > .1 and nlp_model.similarity('food', word) > .15:
         return True
     else:
         return False
 
-@app.route('/get/<word>')
-def get_vec(word):
-    return nlp_model.get_vector(word)
+# if this method is uncommented, the API will not work, could possibly be because the route is not defined
+# @app.route('/get/<word>')
+# def get_vec(word):
+#     return nlp_model.get_vector(word)
 
 if __name__ == '__main__':
     app.run(debug=True)
